500fbf0ba9ec03a7ed4d678711b508111455fd5c
Ariemaharrucha <arie.maharucha.zakka@gmail.com>
2024-12-29 22:51:52 +0700

    add notes and add toats

--- src/components/shared/Layout.tsx
+++ src/components/shared/Layout.tsx
@@ -1,17 +1,20 @@
 import { Box, Center, Flex } from "@chakra-ui/react";
 import { Outlet } from "react-router-dom";
 import { Header } from "./Header";
-
+import { Toaster } from "@/components/ui/toaster";
 
 export const Layout = () => {
   return (
-    <Center boxSizing={'border-box'}>
-      <Flex direction="column" w="4xl" py={5} mx={'auto'} minHeight="100vh">
-        <Header/>
-        <Box as="main" flex="1" py={4}  px={2}>
-          <Outlet />
-        </Box>
-      </Flex>
-    </Center>
+    <>
+      <Toaster />
+      <Center boxSizing={"border-box"}>
+        <Flex direction="column" w="4xl" py={5} mx={"auto"} minHeight="100vh">
+          <Header />
+          <Box as="main" flex="1" py={4} px={2}>
+            <Outlet />
+          </Box>
+        </Flex>
+      </Center>
+    </>
   );
 };
--- src/features/components/AddCard.tsx
+++ src/features/components/AddCard.tsx
@@ -1,5 +1,8 @@
 import { GridItem, Input, Textarea } from "@chakra-ui/react";
 import { Button } from "@/components/ui/button";
+import supabase from "@/config/supabaseClient";
+import { toaster } from "@/components/ui/toaster";
+import { useState } from "react";
 import {
   DialogActionTrigger,
   DialogBody,
@@ -13,6 +16,41 @@ import {
 } from "@/components/ui/dialog";
 
 export const AddNote = () => {
+  const [title, setTitle] = useState<string | null>(null);
+  const [content, setContent] = useState<string | null>(null);
+  const [date, setDate] = useState<string | null>(null);
+  const [isOpen, setIsOpen] = useState<boolean>(false);
+
+  async function handleSaveote() {
+    if (!title || !content || !date) {
+      toaster.error({
+        description: "Fill All Field",
+        duration: 3000,
+      });
+      return;
+    }
+    const { data, error } = await supabase
+      .from("notes")
+      .insert([{ title, note_content: content, date: date }]);
+    console.log(`Triggering toaster.create ${data}`);
+
+    if (error) {
+      toaster.error({
+        description: `${error}`,
+        duration: 3000,
+      });
+      return;
+    }
+
+    toaster.success({
+      description: "File saved successfully",
+      duration: 3000,
+    });
+    setTitle(null);
+    setContent(null);
+    setDate(null);
+    setIsOpen(false);
+  }
   return (
     <GridItem
       colSpan={2}
@@ -26,13 +64,19 @@ export const AddNote = () => {
       alignItems="center"
       borderColor={"blue.300"}
     >
-      <DialogRoot placement={"top"} motionPreset="slide-in-bottom">
+      <DialogRoot
+        placement={"top"}
+        motionPreset="slide-in-bottom"
+        open={isOpen}
+        onOpenChange={(open) => setIsOpen(!!open)}
+      >
         <DialogTrigger asChild>
           <Button
             variant="outline"
             color={"blue.500"}
             fontSize={"md"}
             borderColor={"blue.300"}
+            onClick={() => setIsOpen(true)}
           >
             Add Notes{" "}
           </Button>
@@ -54,6 +98,9 @@ export const AddNote = () => {
                   borderColor: "blue.500",
                   boxShadow: "0 0 0 1px blue.500",
                 }}
+                onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+                  setTitle(e.target.value)
+                }
               />
               <Textarea
                 size="lg"
@@ -66,6 +113,9 @@ export const AddNote = () => {
                   borderColor: "blue.500",
                   boxShadow: "0 0 0 1px blue.500",
                 }}
+                onChange={(e: React.ChangeEvent<HTMLTextAreaElement>) =>
+                  setContent(e.target.value)
+                }
               />
               <Input
                 mt={4}
@@ -74,7 +124,13 @@ export const AddNote = () => {
                 color="black"
                 variant="outline"
                 mb={4}
-                _focus={{ borderColor: "blue.500", boxShadow: "0 0 0 1px blue.500" }}
+                _focus={{
+                  borderColor: "blue.500",
+                  boxShadow: "0 0 0 1px blue.500",
+                }}
+                onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+                  setDate(e.target.value)
+                }
               />
             </form>
           </DialogBody>
@@ -82,7 +138,9 @@ export const AddNote = () => {
             <DialogActionTrigger asChild>
               <Button variant="outline">Cancel</Button>
             </DialogActionTrigger>
-            <Button bg={"blue.600"}>Save</Button>
+            <Button bg={"blue.600"} onClick={handleSaveote}>
+              Save
+            </Button>
           </DialogFooter>
           <DialogCloseTrigger />
         </DialogContent>
