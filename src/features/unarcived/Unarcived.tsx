d48ed523b18e8201202598255ea819e7c2258723
Ariemaharrucha <arie.maharucha.zakka@gmail.com>
2024-12-30 11:32:40 +0700

    add archiive and unarchive

--- src/features/unarcived/Unarcived.tsx
+++ src/features/unarcived/Unarcived.tsx
@@ -1,6 +1,51 @@
+import supabase from "@/config/supabaseClient";
+import { Grid } from "@chakra-ui/react";
+import { useEffect, useState } from "react";
+import { Card } from "../components/Card";
+
+interface INotes {
+  id: number;
+  title: string;
+  note_content: string;
+  date: string;
+  archived: boolean;
+}
 
 export const Unarchived = () => {
+  const [error, setError] = useState<string | null>(null);
+  const [notes, setNotes] = useState<INotes[] | null>(null);
+
+  useEffect(() => {
+    const fetchNotes = async () => {
+      const { data, error } = await supabase
+        .from("notes")
+        .select("*")
+        .eq("archived", "FALSE");
+      if (error) {
+        setError("error fetch data");
+        setNotes(null);
+      }
+      setNotes(data);
+      setError(null);
+    };
+    fetchNotes();
+  }, []);
   return (
-    <div>Unarcived</div>
-  )
-}
+    <Grid templateColumns="repeat(4, 1fr)" gap="6">
+      {error && <p>{error}</p>}
+      {notes &&
+        notes.map((note) => {
+          return (
+            <Card
+              key={note.id}
+              id={note.id}
+              title={note.title}
+              text={note.note_content}
+              date={note.date}
+              archived={note.archived}
+            />
+          );
+        })}
+    </Grid>
+  );
+};
